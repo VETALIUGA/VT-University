b1a76a37de02220a90c4a7db4005ab3fb393c75e
Vitalii <50614752+VETALIUGA@users.noreply.github.com>
2020-01-24 17:41:36 +0200

    Header init

---
+++ src/components/containers/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="section header__section">
+        <div className="container header__container">
+            <div className="grid header__grid">
+                <div className="grid-item header__grid-item">
+
+                </div>
+            </div>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
